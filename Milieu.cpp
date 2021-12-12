e144eda073c322e990d5a5ff5b3c25025c588d17
justinlblc <jleblanc.fra@gmail.com>
2021-12-12 18:08:13 +0100

    création des bestioles dynamiquement dans Milieu::naissanceSPont()

--- Milieu.cpp
+++ Milieu.cpp
@@ -102,23 +102,23 @@ void Milieu::naissanceSpont(){
       int i = std::rand()% 5 +1;
       cout<<"Nombre aléatoire: "<<i<<endl;
       if (i==1 && b1==true){
-         this->addMember(Bestiole(*this, this->greg));
+         this->addMember(*(new Bestiole(*this, this->greg)));
          cout<<"Naissance spontanée Grégaire."<<endl;
       }
       else if (i==2 && b2==true){
-         this->addMember(Bestiole(*this, this->kami));
+         this->addMember(*(new Bestiole(*this, this->kami)));
          cout<<"Naissance spontanée Kamikaze."<<endl;
       }
       else if (i==3 && b3==true){
-         this->addMember(Bestiole(*this, this->peur));
+         this->addMember(*(new Bestiole(*this, this->peur)));
          cout<<"Naissance spontanée Peureuse."<<endl;
       }
       else if (i==4 && b4==true){
-         this->addMember(Bestiole(*this, this->prev));
+         this->addMember(*(new Bestiole(*this, this->prev)));
          cout<<"Naissance spontanée Prévoyante."<<endl;
       }
       else if (i==5){
-         this->addMember(MultiBestiole(*this, b1, b2, b3, b4, this->greg, this->kami, this->peur, this->prev));
+         this->addMember(*(new MultiBestiole(*this, b1, b2, b3, b4, this->greg, this->kami, this->peur, this->prev)));
          cout<<"Naissance spontanée Multibestiole"<<endl;        
       }
       else {
